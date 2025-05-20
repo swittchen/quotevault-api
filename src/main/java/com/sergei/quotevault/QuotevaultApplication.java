edc52ce0184c5f228d3b72473a64aedb0a2bac24
Wittchen <swittche@ipb-halle.de>
2025-05-20 12:36:06 +0200

    init: basic Quote entity and random endpoint

---
+++ src/main/java/com/sergei/quotevault/QuotevaultApplication.java
@@ -0,0 +1,13 @@
+package com.sergei.quotevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotevaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotevaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergei/quotevault/QuotevaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergei.quotevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotevaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
